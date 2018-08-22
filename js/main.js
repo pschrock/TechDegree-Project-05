afc504d3027eb979fa06329150803e8a8ead35e0
Peter Schrock <peter@miskit.org>
2018-08-21 19:33:20 -0700

    Deleted unnecessary files, updated search function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
+
+
 baguetteBox.run('.gallery', {
   captions: true,
   titleTage: false
 });
 
-$(document).ready(function() {
-	$('#search').hideseek({
-    list      :  '.hideseek-data',
-    matches   :  true,
-    attribute :  'data-caption'
+
+//Search
+// Variables
+const $srch = $('#search');
+const $child = $('.list a');
+
+//each key typed triggers evaluation
+$srch.keyup(function () {
+  const $crntsrch = $srch.val().toLowerCase();
+  $child.each(function () {
+    const $str = $(this).attr('data-caption').toLowerCase();
+
+
+//compare text input for match with 'data-caption'
+    if ($str.indexOf($crntsrch) < 0) {
+
+//show only images that match
+      $(this).hide()
+    } else {
+      $(this).show()
+    }
   });
 });
